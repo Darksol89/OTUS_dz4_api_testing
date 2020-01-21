c6f8dba42376243fffb273289ea3f7fb7d531cc7
ichistov <ichistov@eng.vsofts.com>
2020-01-21 19:20:35 +0300

    Файл с фикстурами

---
+++ conftest.py
@@ -0,0 +1,16 @@
+"""Фикстуры для выбора необходимого линка, через коммандную строку"""
+import pytest
+import requests
+
+
+url_dog_ceo = 'https://dog.ceo/dog-api/'
+url_openbrewry = 'https://www.openbrewerydb.org/'
+url_json_placeholder = 'https://jsonplaceholder.typicode.com/'
+
+# def pytest_addoption(parser):
+#     """Парсер для выбора целевого линка до старта теста"""
+#     parser.addoption('--target_url', default='dog_ceo_api', help='User can choice target link before start test')
+
+
+
+
