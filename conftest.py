740fbc465dcbe0ca333d5b7c30ba5d127f47317b
ichistov <ichistov@eng.vsofts.com>
2020-01-21 19:36:04 +0300

    Обновлен
    Добавлен класс и описание функций GET и POST. За основу взята работа с курса

--- conftest.py
+++ conftest.py
@@ -7,9 +7,34 @@ url_dog_ceo = 'https://dog.ceo/dog-api/'
 url_openbrewry = 'https://www.openbrewerydb.org/'
 url_json_placeholder = 'https://jsonplaceholder.typicode.com/'
 
-# def pytest_addoption(parser):
-#     """Парсер для выбора целевого линка до старта теста"""
-#     parser.addoption('--target_url', default='dog_ceo_api', help='User can choice target link before start test')
+class APIClient:
+    """Клиент для рабоыт с API.
+    Анализирует выбранный линк куда посылать запросы"""
+
+    def __init__(self, base_address):
+        self.base_address = base_address
+
+    def post(self, path="/", params=None, data=None, headers=None):
+        url = self.base_address + path
+        print('POST request to {}'.format(url))
+        return requests.post(url=url, params=params, data=data, headers=headers)
+
+    def get(self, path='/', params=None):
+        url = self.base_address + path
+        print("GET request to {}".format(url))
+        return requests.get(url=url, params=params)
+
+def pytest_addoption(parser):
+    """Парсер для выбора целевого линка до старта теста"""
+    parser.addoption('--url',
+                     action='store',
+                     default='https://dog.ceo/dog-api/',
+                     help='Target link for request')
+
+@pytest.fixture(scope='session')
+def api_client(request):
+    base_url = request.config.getoption("--url")
+    return APIClient(base_address=base_url)
 
 
 
