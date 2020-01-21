a757e6e14b51ef5ff69605eb0aa6a1fa94ed4431
Ivan_Chistov <Darksol89@gmail.com>
2020-01-21 22:43:46 +0300

    Обновлены тестовые функции, добавлена параметризация

--- tests/tests_api_service_dogceo.py
+++ tests/tests_api_service_dogceo.py
@@ -1,27 +1,54 @@
+"""Тесты для API на сайте про собак"""
 import pytest
-import requests
-import json
+from jsonschema import validate
 
-# main_page_url = 'https://dog.ceo/dog-api/'
-# breed_list_url = 'https://dog.ceo/api/breeds/list/all'
 
-def test_check_status_code(api_client):
+@pytest.mark.parametrize('status', ['200'])
+def test_api_status_code(api_client, status):
+    """Тест на возвращение статус кода 200"""
     response = api_client.get()
     status_code = response.status_code
-    assert status_code == 200
+    assert status_code == int(status)
 
-def test_json_not_empty(api_client):
+
+def test_api_json_not_empty(api_client):
+    """Тест что json не пустой"""
     response = api_client.get(path='/breeds/image/random')
     assert response.json() != []
 
 
-def test_encoding():
-    response = requests.get(main_page_url)
-    encoding = response.encoding
+@pytest.mark.parametrize('status', ['error'])
+@pytest.mark.parametrize('message', ['No route found for \"POST /api/status/404\" with code: 0'])
+@pytest.mark.parametrize('code', ['404'])
+def test_api_post_request(api_client, status, message, code):
+    """Тест на ошибку 404"""
+    response = api_client.post(path='/status/404',
+                               data={'status': status,
+                                     'message': message,
+                                     'code': code}).json()
 
-    assert encoding in 'UTF-8'
+    assert response['status'] == status
+    assert response['message'] == message
+    assert response['code'] == int(code)
 
-def test_text():
-    response = requests.get(main_page_url)
-    text_list = response.links
-    print(text_list)
+
+@pytest.mark.parametrize('coding', ['UTF-8'])
+def test_api_encoding(api_client, coding):
+    """Тест на проверку кодировки"""
+    response = api_client.get()
+    assert response.encoding in coding
+
+
+def test_api_json_scheme(api_client):
+    """Тест с использованием json схемы"""
+    response = api_client.get('/breeds/image/random')
+    schema = {
+        "type": "object",
+        "properties": {
+            "message": {"type": "string"},
+            "status": {"type": "string"}
+        },
+        "required": ["message", "status"]
+    }
+
+    validate(instance=response.json(), schema=schema)
