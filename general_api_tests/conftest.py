113257401125fdbc02766428d478a5c4c6239960
ichistov <ichistov@eng.vsofts.com>
2020-01-23 15:23:49 +0300

    Фикстура для основных API тестов

---
+++ general_api_tests/conftest.py
@@ -0,0 +1,47 @@
+"""Фикстуры для выбора необходимого линка, через коммандную строку"""
+import pytest
+import requests
+
+url_dog_ceo = 'https://dog.ceo/api'
+url_openbrewry = 'https://api.openbrewerydb.org'
+url_json_placeholder = 'https://jsonplaceholder.typicode.com'
+
+
+class APIClient:
+    """Клиент для рабоыт с API.
+    Анализирует выбранный линк куда посылать запросы"""
+
+    def __init__(self, base_address):
+        self.base_address = base_address
+
+    def post(self, path="/", params=None, data=None, headers=None):
+        url = self.base_address + path
+        print('POST request to {}'.format(url))
+        return requests.post(url=url, params=params, data=data, headers=headers)
+
+    def get(self, path='/', params=None):
+        url = self.base_address + path
+        print("GET request to {}".format(url))
+        return requests.get(url=url, params=params)
+
+
+def pytest_addoption(parser):
+    """Парсер для выбора целевого линка до старта теста"""
+    parser.addoption('--url',
+                     action='store',
+                     default='https://api.openbrewerydb.org',
+                     help='Target link for request')
+
+
+@pytest.fixture(scope='session')
+def api_client(request):
+    base_url = request.config.getoption("--url")
+    if base_url == 'https://dog.ceo/api':
+        print('API general_api_tests running to dog.ceo.api site')
+        return APIClient(base_address=base_url)
+    elif base_url == 'https://api.openbrewerydb.org':
+        print('API general_api_tests running to openbrewery site')
+        return APIClient(base_address=base_url)
+    elif base_url == 'https://jsonplaceholder.typicode.com':
+        print('API general_api_tests running to jsonholderplace site')
+        return APIClient(base_address=base_url)
